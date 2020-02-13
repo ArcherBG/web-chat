0fe7b7163e41cbf90aee60a6467fdb2b1fe9d44d
Kosyo <kosyo@protonmail.com>
2020-02-13 15:23:27 +0200

    added overflow for too long titles

--- src/components/ChatListElement/ChatListElement.js
+++ src/components/ChatListElement/ChatListElement.js
@@ -5,10 +5,10 @@ function ChatListElement(props) {
     const { handleClick, id, title, subtitle } = props;
     return (
         <div className="ChatListElement" onClick={(event) => handleClick(event, id)}>
-            <div className="ChatListElement-title">
+            <div className="ChatListElement-title" title={title}>
                 {title}
             </div>
-            <div className="ChatListElement-subtitle">
+            <div className="ChatListElement-subtitle" title={subtitle}>
                 {subtitle}
             </div>
         </div>
