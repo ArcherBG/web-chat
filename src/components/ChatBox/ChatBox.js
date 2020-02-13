14a530cce1d831598afe63a9cd4c3174c8295ff4
Kosyo <kosyo@protonmail.com>
2020-02-13 10:03:26 +0200

    scroll to bottom of messages

--- src/components/ChatBox/ChatBox.js
+++ src/components/ChatBox/ChatBox.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import './ChatBox.css';
+import React, { useEffect, useRef } from 'react';
 import MessageList from './MessageList/MessageList';
+import './ChatBox.css';
 
 function ChatBox(props) {
     const { messages } = props;
+    const messageEndRef = useRef(null);
+    const scrollToBottom = () => {
+        messageEndRef.current.scrollIntoView({behaviour: "smooth" });
+    }
+
+    useEffect(scrollToBottom, [messages]);
     return (
         <div className="ChatBox">
             <div className="ChatBox-header">Title</div>
             <div className="ChatBox-message-wrapper">
                 <MessageList messages={messages}></MessageList>
+                <div ref={messageEndRef}></div>
             </div>
             <form className="ChatBox-form">
                 <input className="ChatBox-input"></input>
